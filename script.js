929f1bfcdbd429a3fd2a11b52ed58fa2e0d94ad6
Aufa Ikrimah <aufaikrimah@gmail.com>
2023-12-02 17:31:31 +0700

    adding function for side menu

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let topics = ['Menstruation', 'HIV/AIDS', 'Teenage pregnancy', 'Sexual orientation'];
+let selectedCategory = 'beranda';
+let selectedTopic = '';
 
 function displayTopik() {
     const topicList = document.getElementById('topicList');
@@ -7,15 +9,28 @@ function displayTopik() {
     topics.forEach(topic => {
         const listItem = document.createElement('li');
         listItem.innerHTML = `<li class="nav-item">
-            <a class="nav-link text-decoration-none text-body" href="#">${topic}</a>
+            <a class="nav-link text-decoration-none text-body" href="#" data-topic="${topic}">${topic}</a>
         </li>`;
         topicList.appendChild(listItem);
+    });
+
+    // Tambahkan event listener untuk setiap elemen topik
+    topicList.addEventListener('click', function (event) {
+        if (event.target.tagName === 'A') {
+            event.preventDefault();
+
+            // Simpan topik yang dipilih
+            selectedTopic = event.target.dataset.topic;
+
+            // Update konten berdasarkan topik yang dipilih
+            displayDiskusi();
+        }
     });
 }
 
 displayTopik();
 
-// dropdown topik di modal
+//  dropdown topik di modal
 let selectTopics = document.getElementById('inputTopics');
 topics.forEach(topic => {
     let option = document.createElement('option');
@@ -60,7 +75,7 @@ let semuaKonten = [{
         judul: 'Apa benar tidak boleh minum es saat haid?',
         topik: 'Menstruation',
         desk: 'Aku pernah ditegur saat minum es, disitu aku keadaan sedang menstruasi, katanya dapat membekukan pembuluh darah dan membuat haid tidak lancar bla bla bla.',
-        kategori: 'informasi',
+        kategori: 'pertanyaan',
     },
     {
         avatar: 'anjing.jpeg',
@@ -68,6 +83,14 @@ let semuaKonten = [{
         judul: 'Haid tidak lancar setelah melahirkan, normal atau tidak?',
         desk: 'Tidak perlu khawatir, menstruasi tidak lancar setelah melahirkan adalah kondisi normal. Bila ibu memberikan ASI eksklusif selama 6 bulan, umumnya menstruasi akan tidak teratur karena hormon prolaktin berpengaruh pada siklus menstruasi.',
         topik: 'Menstruation',
+        kategori: 'informasi',
+    },
+    {
+        avatar: 'profile.png',
+        nama: 'Dr Boyke',
+        judul: 'Mengenal HIV/AIDS',
+        desk: 'HIV adalah virus yang menyerang sistem kekebalan tubuh manusia, mengganggu kemampuannya untuk melawan infeksi dan penyakit. Jika tidak diobati, HIV dapat berkembang menjadi AIDS.',
+        topik: 'HIV/AIDS',
         kategori: 'informasi',
     },
 ];
@@ -94,7 +117,7 @@ function tambahDiskusi(event) {
         diskusi = {
             judul: judul,
             topik: topik,
-            kategori: 'judul',
+            kategori: 'pertanyaan', //default kategori untuk general user itu semuanya cuma bisa posting pertanyaan
             avatar: selectedAvatar,
             nama: selectedNamaAVatar,
         };
@@ -108,7 +131,7 @@ function tambahDiskusi(event) {
             judul: judul,
             topik: topik,
             desk: desk,
-            diskusi: 'informasi',
+            kategori: 'pertanyaan', //default kategori untuk general user itu semuanya cuma bisa posting pertanyaan
             avatar: selectedAvatar,
             nama: selectedNamaAVatar,
         };
@@ -120,18 +143,60 @@ function tambahDiskusi(event) {
     } else {
         console.log('Isi semua field untuk menambahkan diskusi baru.');
     }
+
+
 }
 
 // Menangani submit form dengan memanggil fungsi tambahDiskusi
 document.getElementById('buatDiskusiBaru').addEventListener('submit', tambahDiskusi);
 
-// Fungsi untuk menampilkan data diskusi ke dalam elemen HTML
+//ini buat link side menu nya
+document.getElementById('link').addEventListener('click', function (event) {
+    if (event.target.tagName === 'A') {
+        event.preventDefault();
+
+        const links = document.querySelectorAll('.links a');
+        links.forEach(link => link.classList.remove('active'));
+
+        event.target.classList.add('active');
+
+        selectedCategory = event.target.dataset.halaman;
+
+        displayDiskusi();
+    }
+});
+
+
+function filterKategori(konten) {
+    return (
+        (selectedCategory === 'beranda') ||
+        (selectedCategory === 'jawab-pertanyaan' && konten.kategori === 'pertanyaan') ||
+        (selectedCategory === 'informasi' && konten.kategori === 'informasi')
+    );
+}
+
+// function filterTopik(konten) {
+//     return (
+//     (selectedTopic === '') ||
+//     (selectedTopic === 'Menstruation' && konten.topik === 'Menstruation') ||
+//     (selectedTopic === 'HIV/AIDS' && konten.topik === 'HIV/AIDS') ||
+//     (selectedTopic === 'Teenage pregnancy' && konten.topik === 'Teenage pregnancy') ||
+//     (selectedTopic === 'Sexual orientation' && konten.topik === 'Sexual orientation'));
+// }
+
 function displayDiskusi() {
     let listKonten = document.getElementById('postCard');
     let diskusiHMTL = '';
 
-    semuaKonten.forEach((konten, index) => {
-        // Membuat elemen list untuk setiap data diskusi
+    const filteredKonten = semuaKonten.filter(konten => {
+        // Filter berdasarkan kategori yang dipilih
+        return filterKategori(konten) //&& filterTopik(konten)
+        ;
+    });
+
+    listKonten.innerHTML = '';
+
+    filteredKonten.forEach((konten, index) => {
         diskusiHMTL += `
             <div class="post-card mb-3">
                 <div class="profile-info">
@@ -147,10 +212,8 @@ function displayDiskusi() {
                         <span class="badge bg-info me-2">${konten.topik}</span>
                         ${konten.desk ? konten.desk : ``}
                     </p>
-
                 </div>
-                <div class="reply-icon" onclick="toggleReplyForm(0)"><span
-                        class="bi bi-chat-right-fill"></span> Balas</div>
+                <div class="reply-icon" onclick="toggleReplyForm(0)"><span class="bi bi-chat-right-fill"></span> Balas</div>
             </div>
             <div class="reply-form bg-white pb-3" style="display: none;">
                 <img src="assets/images/profile.png" alt="Profile Picture">
@@ -159,9 +222,8 @@ function displayDiskusi() {
             </div>
         `;
     });
-    // console.log(diskusiHMTL);
+
     listKonten.innerHTML = diskusiHMTL;
 }
 
-// Panggil fungsi untuk menampilkan data saat halaman dimuat
 window.onload = displayDiskusi();
