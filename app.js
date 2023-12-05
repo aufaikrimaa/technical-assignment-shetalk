a6911908fb625905c7c8a97cacc2b67db659d830
anna <nanamint623@gmail.com>
2023-12-05 20:21:36 +0700

    add transition

---
+++ app.js
@@ -0,0 +1,23 @@
+
+// callback func can observe multiple elements or entries at same time,this func will run anytime
+// the visibility of one the observed elemen changes
+// we use foreach to loop multiple elemen
+
+const observer = new IntersectionObserver((entries)=> {
+    entries.forEach((entry)=>{
+   console.log(entry)
+   if(entry.isIntersecting){
+    entry.target.classList.add('show');
+   }else{
+    entry.target.classList.remove('show');
+   }
+
+    });
+});
+
+
+const hiddenElementsX =  document.querySelectorAll('.hiddenX');
+hiddenElementsX.forEach((el) => observer.observe(el));
+
+const hiddenElementsY =  document.querySelectorAll('.hiddenY');
+hiddenElementsY.forEach((el) => observer.observe(el));
